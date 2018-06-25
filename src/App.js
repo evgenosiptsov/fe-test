347f07dcb60059b27ba2f8b79adec564eb55f739
Evgen Osiptsov <evgen.osiptsov@gmail.com>
2018-06-25 19:23:58 +0300

    init

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Map, TileLayer, GeoJSON } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
+import { DEFAULT_FIELD_STYLE } from './constants/styles'
+import { getColor } from './lib/fields'
 import farm from './data/farm.json';
 import crops from './data/crops.json';
 
@@ -9,24 +11,25 @@ class App extends Component {
   render() {
     return (
       <div>
-        <header>
-          <h1>Welcome to th Hummingbird starter app</h1>
-        </header>
-        <p>
-          To get started, edit <code>src/App.js</code> and save to reload.
-          This is just a starter App, change it, remove things and add things however you want.
-        </p>
         <Map
-          style={{ width: '500px', height: '500px' }}
+          style={{ width: '100%', height: '500px' }}
           center={farm.centre.coordinates}
           zoom={13}
         >
           <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-          {farm.fields.map(field => <GeoJSON key={field.name} data={field.boundary} />)}
+          {farm.fields.map(field => {
+                const fillColor = getColor(100);
+                const style = { DEFAULT_FIELD_STYLE, ...fillColor};
+                return (
+                  <GeoJSON
+                      style={style}
+                      key={field.name}
+                      data={field.boundary}
+                  />
+                )
+              }
+          )}
         </Map>
-        <ul>
-          {crops.map(crop => <li key={crop.name}>{crop.name}</li>)}
-        </ul>
       </div>
     );
   }
