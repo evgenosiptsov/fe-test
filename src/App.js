498569914fb9055026c8161619f9b10cc7344158
Evgen Osiptsov <evgen.osiptsov@gmail.com>
2018-06-25 20:54:03 +0300

    point

--- src/App.js
+++ src/App.js
@@ -4,42 +4,69 @@ import 'leaflet/dist/leaflet.css';
 
 import { DEFAULT_FIELD_STYLE } from './constants/styles'
 import { getColor } from './lib/fields'
+import SimpleControl from './components/SimpleControl'
 import farm from './data/farm.json';
 import crops from './data/crops.json';
 
-const { BaseLayer, Overlay } = LayersControl;
+const { Overlay } = LayersControl;
 
 class App extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = { width: '100%', height: '800px' };
+    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+  }
+  
+  componentWillMount() {
+    this.updateWindowDimensions();
+    window.addEventListener('resize', this.updateWindowDimensions);
+  }
+  
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateWindowDimensions);
+  }
+  
+  updateWindowDimensions() {
+    this.setState({ width: window.innerWidth, height: window.innerHeight });
+  }
+  
   render() {
+    const {width, height} = this.state;
+    
     return (
         <Map
-          style={{ width: '100%', height: '800px' }}
+          style={{ width, height }}
           center={farm.centre.coordinates}
           zoom={13}
         >
- 
-          
-          <LayersControl position="bottomleft">
-            
-            <Overlay name="Open Street" checked={true}>
-              <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-            </Overlay>
+          <LayersControl position="topright">
+  
+              <SimpleControl position="topright" >
+                <h1>!!!</h1>
+              </SimpleControl>
             
               {farm.fields.map(field => {
-                    const fillColor = getColor(100);
-                    const style = { DEFAULT_FIELD_STYLE, ...fillColor};
-                    return (
-                        <BaseLayer name={field.name} checked={true}>
-                          <GeoJSON
-                              style={style}
-                              key={field.name}
-                              data={field.boundary}
-                              checked={true}
-                          />
-                        </BaseLayer>
-                    )
-                  }
+                  const fillColor = getColor(100);
+                  const style = { DEFAULT_FIELD_STYLE, ...fillColor};
+                  return (
+                      <Overlay name={field.name} checked={true}>
+                        <GeoJSON
+                            style={style}
+                            key={field.name}
+                            data={field.boundary}
+                            checked={true}
+                        />
+                      </Overlay>
+                  )
+                }
               )}
+  
+            <SimpleControl position="bottomright" >
+              <h1>!!!</h1>
+            </SimpleControl>
+  
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
             
           </LayersControl>
         </Map>
---
+++ src/components/SimpleControl.js
@@ -0,0 +1,75 @@
+import { Component, Children } from 'react';
+import { Control, DomUtil, DomEvent } from 'leaflet';
+import { MapControl } from 'react-leaflet';
+import PropTypes from 'prop-types';
+import { Map } from 'leaflet';
+import { render, unmountComponentAtNode } from 'react-dom';
+
+const leafletControl = Control.extend({
+	options: {
+		className: '',
+		onOff: '',
+		handleOff: function noop(){}
+	},
+	
+	onAdd: function (map) {
+		var _controlDiv = DomUtil.create('div', this.options.className);
+		DomEvent.disableClickPropagation(_controlDiv);
+		return _controlDiv;
+	},
+	
+	onRemove: function (map) {
+		if (this.options.onOff) {
+			map.off(this.options.onOff, this.options.handleOff, this);
+		}
+		return this;
+	}
+});
+
+export default class SimpleControl extends MapControl {
+	static contextTypes = MapControl.contextTypes;
+	static childContextTypes = MapControl.childContextTypes;
+	static propTypes = {
+		children: PropTypes.node,
+		map: PropTypes.instanceOf(Map),
+		popupContainer: PropTypes.object,
+		position: PropTypes.string
+	};
+	
+	constructor(props){
+		super(props);
+	}
+	
+	componentWillMount() {
+		const { children: _children, map: _map, popupContainer, ...props } = this.props;
+		
+		this.leafletElement = new leafletControl(props);
+	}
+	
+	componentDidMount(){
+		super.componentDidMount();
+		this.renderContent();
+	}
+	
+	componentDidUpdate(next) {
+		super.componentDidUpdate(next);
+		this.renderContent();
+	}
+	
+	componentWillUnmount() {
+		unmountComponentAtNode(this.leafletElement.getContainer());
+	}
+	
+	renderContent() {
+		const container = this.leafletElement.getContainer();
+		render(
+			Children.only(this.props.children),
+			container
+		);
+	}
+	
+	render() {
+		return null;
+	}
+	
+}
