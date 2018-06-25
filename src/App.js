5a92ef3127d3b1b16a75fb43bab3d96198ac95f7
Evgen Osiptsov <evgen.osiptsov@gmail.com>
2018-06-25 19:55:57 +0300

    point

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Map, TileLayer, GeoJSON } from 'react-leaflet';
+import { Map, TileLayer, GeoJSON, LayersControl, ScaleControl, ZoomControl } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
 import { DEFAULT_FIELD_STYLE } from './constants/styles'
@@ -7,30 +7,42 @@ import { getColor } from './lib/fields'
 import farm from './data/farm.json';
 import crops from './data/crops.json';
 
+const { BaseLayer, Overlay } = LayersControl;
+
 class App extends Component {
   render() {
     return (
-      <div>
         <Map
-          style={{ width: '100%', height: '500px' }}
+          style={{ width: '100%', height: '800px' }}
           center={farm.centre.coordinates}
           zoom={13}
         >
-          <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-          {farm.fields.map(field => {
-                const fillColor = getColor(100);
-                const style = { DEFAULT_FIELD_STYLE, ...fillColor};
-                return (
-                  <GeoJSON
-                      style={style}
-                      key={field.name}
-                      data={field.boundary}
-                  />
-                )
-              }
-          )}
+ 
+          
+          <LayersControl position="bottomleft">
+            
+            <Overlay name="Open Street" checked={true}>
+              <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            </Overlay>
+            
+              {farm.fields.map(field => {
+                    const fillColor = getColor(100);
+                    const style = { DEFAULT_FIELD_STYLE, ...fillColor};
+                    return (
+                        <BaseLayer name={field.name} checked={true}>
+                          <GeoJSON
+                              style={style}
+                              key={field.name}
+                              data={field.boundary}
+                              checked={true}
+                          />
+                        </BaseLayer>
+                    )
+                  }
+              )}
+            
+          </LayersControl>
         </Map>
-      </div>
     );
   }
 }
---
+++ src/constants/styles.js
@@ -0,0 +1,9 @@
+export const DEFAULT_FIELD_STYLE = {
+	fillColor: 'green',
+	weight: 2,
+	opacity: 1,
+	color: 'white',
+	dashArray: '3',
+	fillOpacity: 0.7
+}
+
